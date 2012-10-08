b275cec2f3829d87383823c3ad90208a31eae0e7
Braulio Bhavamitra <brauliobo@gmail.com>
2012-10-08 11:50:12 +0000

    Remove unecessary strip

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -31,7 +31,7 @@ class String
   end
 
   def filter_normalization
-    self.strip.squish.gsub(/\b(s.a|s\/a|sa)\.?$/i, '').strip.transliterate.downcase
+    self.squish.gsub(/\b(s.a|s\/a|sa)\.?$/i, '').strip.transliterate.downcase
   end
 
 end
