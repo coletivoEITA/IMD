db5694e08c7ea38cb0ab13cdce60d94afd5977ea
Braulio Bhavamitra <brauliobo@gmail.com>
2012-10-17 14:06:26 -0400

    Fix mechanize caching

--- app/models/cache.rb
+++ app/models/cache.rb
@@ -10,11 +10,12 @@ class Cache
 
   def self.request_page(mech, method, *args)
     identifier = {:method => method, :args => args}
-    cache = self.find_by_identifier identifier
+    cache = self.first :identifier => identifier
 
     if cache
       uri = URI.parse args.first
       page = Mechanize::Page.new uri, nil, cache.content, nil, mech
+      puts "Cache hit for #{uri}"
     else
       page = mech.send("#{method}_without_cache", *args)
       Cache.create! :identifier => identifier, :content => page.body
@@ -38,8 +39,18 @@ class Cache
   def self.disable
     Methods.each do |method|
       next unless Mechanize.respond_to?("#{method}_with_cache")
-      Mechanize.send :alias_method, "#{method}_with_cache", method
+      Mechanize.send :alias_method, "#{method}_without_cache", method
     end
   end
+
+  def self.test
+    Cache.enable
+    m = Mechanize.new
+    m.get 'http://kernel.org'
+    m.get 'http://kernel.org'
+    true
+  end
+
+  protected
 
 end
